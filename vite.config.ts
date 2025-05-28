881810fcc61fa01f7f7eab025ebf396d8d84f786
andrewgremlich <andrew.gremlich@gmail.com>
2025-05-28 13:28:34 -0600

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-/// <reference types='vitest' />
 import { defineConfig } from "vite";
 import { vercelPreset } from "@vercel/react-router/vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(() => ({
-	root: __dirname,
-	cacheDir: "../../node_modules/.vite/apps/ai-freelance-dev",
 	server: {
 		port: 4200,
 		host: "localhost",
