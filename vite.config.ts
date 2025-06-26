3c3e65e6ff69da59b983e1e04bf2691be8822c92
andrewgremlich <andrew.gremlich@gmail.com>
2025-06-26 17:57:00 -0600

    vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
 import { vercelPreset } from "@vercel/react-router/vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig(() => ({
-	server: {
-		port: 4200,
-		host: "localhost",
-	},
-	base: "/",
-	preview: {
-		port: 4300,
-		host: "localhost",
-	},
-	plugins: [react()],
-	presets: [vercelPreset()],
-	build: {
-		outDir: "./dist",
-		emptyOutDir: true,
-		reportCompressedSize: true,
-	},
+  server: {
+    port: 4200,
+    host: "localhost",
+  },
+  base: "/",
+  preview: {
+    port: 4300,
+    host: "localhost",
+  },
+  plugins: [react()],
+  presets: [vercelPreset()],
+  build: {
+    outDir: "./dist",
+    emptyOutDir: true,
+    reportCompressedSize: true,
+  },
 }));
