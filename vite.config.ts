76eedc35577f0605459f25ebacf5f24411c5a193
andrewgremlich <andrew.gremlich@gmail.com>
2025-07-04 17:47:41 -0600

    updating vite build

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,26 @@ export default defineConfig(() => ({
   plugins: [react()],
   presets: [vercelPreset()],
   build: {
+    minify: true,
+    target: "es2022",
     outDir: "./dist",
     emptyOutDir: true,
     reportCompressedSize: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name][extname]",
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            const parts = id.split("node_modules/")[1].split("/");
+            const pkgName = parts[0].startsWith("@")
+              ? `${parts[0]}/${parts[1]}`
+              : parts[0];
+            return `vendor/${pkgName}`;
+          }
+        },
+      },
+    },
   },
 }));
