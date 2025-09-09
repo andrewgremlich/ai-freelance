1e676e7bcfd6ad9a125d5b837832b297bcebf800
andrewgremlich <andrew.gremlich@gmail.com>
2025-09-08 21:29:38 -0600

    removing rain from meditate because too distracting or too loud.

--- src/app/slides/Meditate/index.tsx
+++ src/app/slides/Meditate/index.tsx
@@ -1,15 +1,6 @@
-import { useAudio } from "summit-kit/client";
-
 import classes from "./Meditate.module.css";
 
 export const Meditate = () => {
-	useAudio({
-		src: "calming-rain.webm",
-		volume: 0.5,
-		autoplay: true,
-		loop: true,
-	});
-
 	return (
 		<div className={classes.meditateContainer}>
 			<div className={classes.underlay} />
